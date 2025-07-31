5b88ebbeb5e864352786e79e3651f754a32ea71a
_Nixo_ <113860216+Nixo0725@users.noreply.github.com>
2025-07-31 02:08:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
-// Script vide pour l'instant
-console.log("Site chargé !");
+// Simple script pour afficher un message dans la console
+
+console.log("Bienvenue sur le portfolio de Robin Girbal !");
